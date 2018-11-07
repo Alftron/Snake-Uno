d01abbd425a066a66480162204c33d90c8818317
Alftron <mja.haycock@gmail.com>
2018-11-07 08:21:16 +0000

    Moved defines into separate header file, not sure if this will work in VSCode though, why would it.........

--- src/main.ino
+++ src/main.ino
@@ -1,48 +1,7 @@
 #include <Elegoo_GFX.h>
 #include <Elegoo_TFTLCD.h>
 #include <TouchScreen.h>
-
-// Pin defines
-// These are the pins for the shield!
-#define YP A3  // must be an analog pin, use "An" notation!
-#define XM A2  // must be an analog pin, use "An" notation!
-#define YM 9   // can be a digital pin
-#define XP 8   // can be a digital pin
-#define LCD_CS A3
-#define LCD_CD A2
-#define LCD_WR A1
-#define LCD_RD A0
-#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
-
-// Assign human-readable names to some common 16-bit color values:
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
-
-// LCD details
-#define LCDIDENTIFIER 0x9341
-#define LANDSCAPE_USBL 0x03
-#define LANDSCAPE_USBR 0x01
-
-// Button defines
-#define BUTTON_X 140
-#define BUTTON_Y 100
-#define BUTTON_W 200
-#define BUTTON_H 120
-#define BUTTON_TEXTSIZE 3
-
-// Touchscreen defines
-#define MINPRESSURE 10
-#define MAXPRESSURE 1000
-#define TS_MINX 70
-#define TS_MAXX 920
-#define TS_MINY 120
-#define TS_MAXY 900
+#include "defines.h"
 
 // LCD business
 Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
